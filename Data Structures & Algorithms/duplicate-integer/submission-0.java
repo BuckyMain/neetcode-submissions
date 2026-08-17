0aabe8db43beaaebba8b9327452f77d5ebcf5cee
Manuel Marti <manuelalanmarti@gmail.com>
2026-08-17 18:19:36 +0200

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        
+        HashSet set = new HashSet<>();
+        
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        if (set.size() != nums.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
